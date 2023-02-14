e74c516acc6e38ec001876aa71f1f31ac9d2c97c
Wim Van Laer <van.laer.wim@gmail.com>
2023-02-14 18:21:58 +0100

    fixes

--- lib/flutter_fox_logging.dart
+++ lib/flutter_fox_logging.dart
@@ -3,5 +3,6 @@ library flutter_fox_logging;
 export 'package:flutter_fox_logging/src/level_converter/log_level_to_color_converter.dart';
 export 'package:flutter_fox_logging/src/level_converter/log_level_to_icon_converter.dart';
 export 'package:flutter_fox_logging/src/logs_controller/logs_controller.dart';
+export 'package:flutter_fox_logging/src/logs_controller/streamed_logs_controller.dart';
 export 'package:flutter_fox_logging/src/logs_screen.dart';
 export 'package:fox_logging/fox_logging.dart';
